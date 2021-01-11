081e800c9179c686c908f6665c228f9a183bcecd
Joseph Avila Alvarez <josdavil@espol.edu.ec>
2021-01-11 02:17:02 -0500

    add:showstreamlines

--- taugeometric/tortuosity/__funcs__.py
+++ taugeometric/tortuosity/__funcs__.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 from taugeometric.poresSE import points_start
 from taugeometric.poresSE import points_end
 from taugeometric.astar import astar
@@ -15,20 +17,40 @@ def metric_euclidian(points):
 
 
     
+def show_streamlines(maze,paths):
+    plt.imshow(maze,cmap="gray_r")
+    for i in paths:
+        for j in i:
+            maze[j[0]][j[1]]=2
+    colors = 'white dimgray black '.split()
+    cmap = matplotlib.colors.ListedColormap(colors, name='colors', N=None)
+    plt.imshow(maze,cmap=cmap)
     
-def tortuosity_geometric_2d(maze):
+def tortuosity_geometric_2d(maze,showStreamline=False):
     line_maze_h=maze.shape[1]-1
     p_s = points_start(maze)
     p_e = points_end(maze)
 
     f_data = []
+    path_show=[]
     for i in p_s:
         selection_path=[]
+        
+        pahts=[]
         for j in p_e:
             data = astar(maze, i, j)
             if(data != None):
                 selection_path.append(metric_euclidian(data))
-        
-        f_data.append(np.array(selection_path).min())
+                pahts.append(data)
+                
+        minimum=np.array(selection_path).min()
+        position=selection_path.index(minimum)
+        path_show.append(pahts[position])
+        f_data.append(minimum)
+    if(showStreamline):
+        show_streamlines(maze,path_show)
     t_g=np.mean(f_data)/(line_maze_h)
     return t_g
+
+
+    
