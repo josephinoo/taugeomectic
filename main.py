a2299d868d4c433d7bb85218c5c93c04aea1b105
Joseph avila <josdavil@espol.edu.ec>
2021-05-16 19:49:36 -0500

    add:datasets and notebooks

--- main.py
+++
@@ -1,9 +0,0 @@
-import taugeometric as tg
-import porespy as ps
-import numpy as np
-
-im = ps.generators.blobs(shape=[50, 50], porosity=0.8, blobiness=0.5)
-maze = np.logical_not(im)
-maze = np.array(maze, dtype=int)
-
-print(tg.tortuosity.tortuosity_geometric_2d(maze))
